81e9283d7b346e2b37c8fe6f84698fe04be85fa4
jinal <jilu.jr11@gmail.com>
2024-12-10 14:16:47 +0530

    feed feature

--- src/utils/appStore.js
+++ src/utils/appStore.js
@@ -1,11 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
 import userReducer from "../utils/userSlice.js";
+import feedReducer from "../utils/feedSlice.js"
 
 
 
 const appStore = configureStore({
     reducer: {
         user: userReducer,
+        feed: feedReducer, 
     },
 })
 
