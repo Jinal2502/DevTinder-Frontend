a1969da7fe28a65d9021da11c3c51b7121d50eb4
jinal <jilu.jr11@gmail.com>
2024-12-10 19:34:16 +0530

    user card

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -3,35 +3,35 @@ import React, { useEffect } from 'react'
 import { BASE_URL } from '../utils/constants'
 import { useDispatch, useSelector } from 'react-redux'
 import { addFeed } from '../utils/feedSlice'
+import UserCard from './UserCard'
 
 const Feed = () => {
-  
   const feed = useSelector((store) => store.feed)
   const dispatch = useDispatch();
 
   const getFeed = async () => {
-    if(feed)return;
+    if(feed && feed.data && feed.data.length > 0) return;
 
     try {
       const res =  await axios.get(BASE_URL + "/feed", {withCredentials:true});
       dispatch(addFeed(res.data));
     } catch (err) {
       console.error(err);
-      
     }
-
   }
 
   useEffect(() => {
-      getFeed();
+    getFeed();
   }, []);
 
-
-
-
   return (
-    <div>Feed</div>
-  )
-}
-
+    feed && feed.data && feed.data.length > 0 ? (
+      <div> 
+        <UserCard user={feed.data[0]} />
+      </div>
+    ) : (
+      <div>Loading...</div>
+    )
+  );
+};
 export default Feed
